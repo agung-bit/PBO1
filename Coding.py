bddb299d41c0310f8fd6aa4143d0ec3dffa3aebe
it's Rafli <57083813+raflix1@users.noreply.github.com>
2020-12-28 15:17:12 +0700

    update complete

--- Coding.py
+++ Coding.py
@@ -12,6 +12,7 @@ class Klinik:
         con.commit()
         con.close()
         print("Akun Telah Sukses Dibuat")
+        Akun.masuk_akun()
     
     def Login(Username, Password):
         con = sqlite3.connect("database.db")
@@ -21,15 +22,10 @@ class Klinik:
         query = query % (Username, Password)
         cursor.execute(query)
         con.commit()
-        rows = cursor.fetchall()
-        accept_Login = True
-        if (len(rows)) == 0:
-            accept_Login = False
-
-        if accept_Login == False:
+        row = cursor.fetchall()
+        if (len(row)) == 0:
             print("Maaf Login Gagal, Mohon cek username & password anda")
-        elif Username == rows[0][0] and Password == rows[0][1]:
-            accept_Login = True
+        elif Username == row[0][0] and Password == row[0][1]:
             print("Login Sukses")
             Akun.program(Username)      
         con.close()
@@ -44,14 +40,28 @@ class Klinik:
         con.commit()
         con.close()
         print("Data pasien berhasil diunggah")
+        Akun.program_pegawai()
+
+    def dataPasien():
+        con = sqlite3.connect("database.db")
+        cursor = con.cursor()
+        query = 'SELECT * FROM [Data Klinik "Happy Health"]'
+        cursor.execute(query)
+        hasil = cursor.fetchall()
+        con.commit()
+        for i in hasil:
+            print(i)
+        con.close()
+        Akun.program_pegawai()
 
 class Akun:
+
     def masuk_akun():
         print('selamat datang di program klinik mata')
         print("""silahkan pilih angka sesuai fitur di bawah
         1. Buat akun baru
         2. Login
-        3. batal""")
+        3. batal / keluar""")
         a=input('mohon input fitur sesuai angka :')
         if a=='1':
             print('Anda akan membuat akun baru, silahkan isi datadiri anda')
@@ -78,5 +88,109 @@ class Akun:
         rows = cursor.fetchall()
         print('selamat datang',rows[0][0])
         con.close()
-      
+        Akun.program_pegawai()
+
+    def program_pegawai():
+        print("""silahkan pilih angka sesuai fitur di bawah
+        1. Administrasi Pasien
+        2. Data Pasien
+        3. Menu Data Obat
+        4. kembali ke menu akun
+        5. keluar""")
+        a=input('mohon input fitur sesuai angka :')
+        if a=='1':
+            print('Silahkan isi data data pasien dibawah ini')
+            Nama = input('nama pasien :')
+            Jenis_Kelamin = input('jenis kelamin :')
+            Usia = input('usia :')
+            Alamat = input('alamat :')
+            Status_Kewarganegaraan = input('status kewarganegaraan :')
+            Tempat_tanggal_lahir = input('tempat, tanggal lahir :')
+            Penyakit = input('penyakit yang diderita :')
+            Dokter = input('dokter yang menangani :')
+            Agama = input('agama :')
+            No_telepon = input('no.telepon :')
+            Tanggal_Periksa = input('tanggal periksa :')
+            Jenis_Obat = input('jenis obat :')
+            Klinik.Administrasi(Nama, Jenis_Kelamin, Usia, Alamat, Status_Kewarganegaraan, Tempat_tanggal_lahir, Penyakit, Dokter, Agama, No_telepon, Tanggal_Periksa, Jenis_Obat)
+        elif a=="2":
+            Klinik.dataPasien()
+        elif a=='3':
+            Akun.obat()
+        elif a=='4':
+            Akun.masuk_akun()
+        elif a=='5':
+            print('terimakasih telah memakai program ini :)')
+        else:print('inputan anda salah, mohon mulai ulang program')
+
+    def obat():
+        print("""silahkan pilih angka sesuai fitur di bawah
+        1. Lihat stock obat
+        2. Tambah data obat
+        3. Edit data obat
+        4. kembali ke menu pegawai
+        5. keluar""")
+        a=input('mohon input fitur sesuai angka :')
+        if a=='1':
+            print('Data Obat')
+            obat.lihat()
+        elif a=='2':
+            print('input nama & jumlah stock obat')
+            Nama_Obat = input('Nama obat :')
+            Jumlah_Stock = input('stock obat :')
+            obat.tambah(Nama_Obat, Jumlah_Stock)
+        elif a=='3':
+            print('input nama obat yang mau diedit stocknya')
+            Nama_Obat = input('Nama obat :')
+            obat.edit(Nama_Obat)
+        elif a=='4':
+            Akun.program_pegawai()
+        elif a=='5':
+            print('terimakasih telah memakai program ini :)')
+        else:print('inputan anda salah, mohon mulai ulang program')
+
+class obat():
+
+    def lihat():
+        con = sqlite3.connect("database.db")
+        cursor = con.cursor()
+        query = 'SELECT * FROM [Data Obat]'
+        cursor.execute(query)
+        hasil = cursor.fetchall()
+        con.commit()
+        for i in hasil:
+            print(i)
+        Akun.obat()
+
+    def tambah(Nama_Obat, Jumlah_Stock):
+        con = sqlite3.connect("database.db")
+        cursor = con.cursor()
+        query = 'INSERT INTO [Data Obat] ([Nama Obat], [Jumlah Stock]) VALUES (\'%s\', \'%s\')' 
+        query = query % (Nama_Obat, Jumlah_Stock)
+        cursor.execute(query)
+        con.commit()
+        con.close()
+        print("Data obat telah berhasil ditambah")
+        Akun.obat()
+    
+    def edit(Nama_Obat):
+        con = sqlite3.connect("database.db")
+        cursor = con.cursor()
+        query = 'SELECT [Nama Obat], [Jumlah Stock] FROM [Data Obat] where [Nama Obat]=\'%s\' '
+        query = query % (Nama_Obat)
+        cursor.execute(query)
+        con.commit()
+        row = cursor.fetchall()
+        stock = input('input jumlah stock barang yang baru :')
+        baru = stock[0][0]
+        baru = stock
+        query = 'UPDATE [Data Obat] SET [Jumlah Stock] = \'%s\' Where [Nama Obat] = \'%s\' ' 
+        query = query % (baru, Nama_Obat)
+        cursor.execute(query)
+        con.commit()
+        con.close()
+        print("Data obat telah berhasil ditambah")
+        Akun.obat()
+
+#administrasi jenis obat
 Akun.masuk_akun()
